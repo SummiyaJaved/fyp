b717381b9b70c2a5f3b4b3c296aecab9ce3e5655
Summiya Javed <summiyajaved13@gmail.com>
2021-05-15 22:27:13 +0500

    Following things are done: - new directory common-widgets created - new circular-avatar file added - Reformat the code

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fyp/ui/common-widgets/circular-avatar.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -14,12 +13,9 @@ class SplashScreen extends StatelessWidget {
             Container(
               padding: EdgeInsets.symmetric(vertical: 20.0),
               color: Color(0xff5a8d49),
-              child: CircleAvatar(
+              child: CircularAvatar(
+                child: Image.asset('images/logo.png'),
                 radius: 140.0,
-                backgroundColor: Colors.white,
-                child: Image.asset(
-                  'images/logo.png',
-                ),
               ),
             ),
             Text(
@@ -30,48 +26,58 @@ class SplashScreen extends StatelessWidget {
                   color: Color(0xff5a8d49)),
               textAlign: TextAlign.center,
             ),
-            SizedBox(height: 10.0,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  radius: 100.0,
-                  backgroundColor: Color(0xff5a8d49),
-                  child: Text(
-                    'Image-1',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 24.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                CircleAvatar(
-                  radius: 100.0,
-                  backgroundColor: Color(0xff5a8d49),
-                  child: Text(
-                    'Image-2',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 24.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
+            SizedBox(
+              height: 10.0,
             ),
-            CircleAvatar(
-              radius: 100.0,
-              backgroundColor: Color(0xff5a8d49),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircularAvatar(
+                    /*
+                    * I have used a text here instead of images
+                    * Because you are using it in the form of Circle so images should be PNG
+                    * For Example download the image logo.png from images folder to see format
+                    * When you will have your images in the correct form use Widget(CircleAvatar) as i did at line 18
+                    * */
+                    child: Text(
+                      'Image-1',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                    backgroundColor: Color(0xff5a8d49),
+                    radius: 100.0,
+                  ),
+                  CircularAvatar(
+                    child: Text(
+                      'Image-2',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                    backgroundColor: Color(0xff5a8d49),
+                    radius: 100.0,
+                  ),
+                ],
+              ),
+            ),
+            CircularAvatar(
               child: Text(
                 'Image-3',
                 style: TextStyle(
                   fontWeight: FontWeight.w900,
-                  fontSize: 24.0,
                   color: Colors.white,
+                  fontSize: 24.0,
                 ),
               ),
+              backgroundColor: Color(0xff5a8d49),
+              radius: 100.0,
             ),
             SizedBox(
               height: 80.0,
@@ -80,15 +86,15 @@ class SplashScreen extends StatelessWidget {
         ),
         floatingActionButton: MaterialButton(
           child: Text(
-              'Next',
+            'Next',
             style: TextStyle(
               fontSize: 18.0,
               color: Colors.white,
               fontWeight: FontWeight.w900,
             ),
           ),
-          onPressed: (){
-          //  TODO: Add here Navigator
+          onPressed: () {
+            //  TODO: Add here Navigator
           },
           color: Color(0xff5a8d49),
           height: 50.0,
